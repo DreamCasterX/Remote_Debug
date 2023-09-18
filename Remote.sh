7f9a7e1479133fdf0b7cb20a937b665f76f8268d
Mike Lu <mcumaple@hotmail.com>
2023-09-18 14:51:02 +0800

    feat: disable auto suspend

--- Remote.sh
+++ Remote.sh
@@ -2,17 +2,19 @@
 
 
 # CREATOR: mike.lu@hp.com
-# CHANGE DATE: 2023/9/15
+# CHANGE DATE: 2023/9/18
 
 
-# [Note]
-# Please provide the login password for the current user
-# Please provide the ssh session string, (e.g., 'ssh xxxxxxx.tmate.io')
-# You can close the terminal after providing the ssh session string
-# If you reboot the system, the ssh session string will change
+<<COMMENT
+[Note]
+1. Please provide the login user name and password.
+2. Please provide the ssh session string, (e.g., 'ssh xxxxxxx.tmate.io').
+3. You can close the Terminal window after saving the ssh session string.
+4. The ssh session string will be renewed whenever you reboot the system or re-run this script.
+COMMENT
 
 
-gnome-terminal --maximize -x bash -c 'nslookup "hp.com";if [ $? != 0 ]; then echo "No Internet connection!" && sleep 5 && exit 0; fi; [[ -f /usr/bin/ssh ]] || sudo apt update && sudo apt install ssh -y; [[ -f /usr/bin/tmate ]] || sudo apt update && sudo apt install tmate -y; [[ -f /usr/bin/sosreport ]] || sudo apt update && sudo apt install sosreport -y; ss -tpun | grep -w tmate; if [ $? != 0 ]; then tmate; fi'
-
+# Run as a program
+gnome-terminal --maximize -x bash -c 'nslookup "hp.com"; if [ $? != 0 ]; then echo "No Internet connection!" && sleep 5 && exit 0; fi; gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type "nothing"; gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-type "nothing"; [[ -f /usr/bin/ssh ]] || sudo apt update && sudo apt install ssh -y; [[ -f /usr/bin/tmate ]] || sudo apt update && sudo apt install tmate -y; [[ -f /usr/bin/sosreport ]] || sudo apt update && sudo apt install sosreport -y; ss -tpun | grep -w tmate; if [ $? != 0 ]; then tmate; else killall tmate && tmate; fi'
 
 
